140f24d4b05e66f53625fe0eae81d9e2cc4713b6
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-05-28 00:53:32 +0200

    Create backup-volume.ps1

---
+++ backup-volume.ps1
@@ -0,0 +1,7 @@
+docker run --rm -it `
+    -v $env:USERPROFILE/.config/rclone:/config:ro `
+    -v blog-paesa-es_data:/var/lib/ghost/content:ro `
+    rclone/rclone `
+    --config /config/rclone.conf `
+    sync /var/lib/ghost/content gdrive:/blog-paesa-es `
+    --progress
