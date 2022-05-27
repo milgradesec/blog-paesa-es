49863d844ee7a981ccb6216ee53a3040425715f9
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-05-28 01:30:36 +0200

    Create make.ps1

--- make.ps1
+++ make.ps1
@@ -1,3 +1,15 @@
+# Generate static site
+gssg --url https://blog.paesa.es --dest static
+
+# Add custom headers
+Copy-Item -Path "_headers" -Destination "static/_headers"
+
+# Fetch missing assets
+Invoke-WebRequest -Uri "http://localhost:2368/assets/built/casper.js" -OutFile static/assets/built/casper.js
+Invoke-WebRequest -Uri "http://localhost:2368/assets/built/screen.css" -OutFile static/assets/built/screen.css
+Invoke-WebRequest -Uri "http://localhost:2368/public/cards.min.js" -OutFile static/public/cards.min.js
+Invoke-WebRequest -Uri "http://localhost:2368/public/cards.min.css" -OutFile static/public/cards.min.css
+
 $files = Get-ChildItem .\static *.html -Recurse
 foreach ($file in $files) {
     (Get-Content $file.PSPath) |
