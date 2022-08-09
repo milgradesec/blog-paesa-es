227833987a22c3500528f921a918fd0d43609699
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-08-09 10:43:49 +0200

    Update backup-volume.ps1

--- backup-volume.ps1
+++ backup-volume.ps1
@@ -2,7 +2,7 @@ docker --context default `
     run --rm -it `
     -v $env:USERPROFILE/.config/rclone:/config:ro `
     -v blog-paesa-es_data:/var/lib/ghost/content:ro `
-    rclone/rclone:1.59 `
+    rclone/rclone:1.59.1 `
     --config /config/rclone.conf `
     sync /var/lib/ghost/content gdrive:/blog-paesa-es `
     --progress
